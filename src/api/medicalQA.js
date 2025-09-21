ae55e3766a6c4b50b0daa63b629db8b8a173d753
lpf-330 <Lpf7896325@outlook.com>
2025-09-21 23:29:43 +0800

    修补

--- src/api/medicalQA.js
+++ src/api/medicalQA.js
@@ -1,4 +1,4 @@
-import { httpService } from "../utils/request"
+import { httpService } from "../utils/apiService"
 
 const userQuery = async (question) => {
     return (await httpService.post('/api/query', { question: question })).data
